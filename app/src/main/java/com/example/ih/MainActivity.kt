aff9e73884d273dd2caf6e885c25ca3702a9cb10
rahmiap4 <2110031805009@sar.ac.id>
2023-03-28 22:01:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ih/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.ih
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var btnIntent: Button
+    private lateinit var btnpdata: Button
+    private lateinit var btnpobjek: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnIntent =findViewById(R.id.btn_intent)
+        btnpdata=findViewById(R.id.btn_pindah_data)
+        btnpobjek=findViewById(R.id.btn_pindah_objek)
+
+        btnIntent.setOnClickListener(this)
+        btnpdata.setOnClickListener(this)
+        btnpobjek.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btn_intent-> run{
+                val intentBiasa = Intent(this@MainActivity, MainActivity2::class.java)
+                startActivity(intentBiasa)
+            }
+            R.id.btn_pindah_data-> run{
+                val intentData = Intent(this@MainActivity, PindahDataActivity::class.java)
+                intentData.putExtra(PindahDataActivity.EXTRA_TEXT, "ayo belajar kotlin")
+                startActivity(intentData)
+            }
+            R.id.btn_pindah_objek->run{
+                val cars = Cars(
+                    "Honda Civic",
+                    2020,
+                    "BM 2121 RAI",
+                )
+                val intentobjek = Intent(this@MainActivity, PindahObjekActivity::class.java)
+                intentobjek.putExtra(PindahObjekActivity.EXTRA_CARS,cars)
+                startActivity(intentobjek)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/ih/PindahObjekActivity.kt
@@ -0,0 +1,23 @@
+package com.example.ih
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class PindahObjekActivity : AppCompatActivity() {
+    private lateinit var tvpenerimaobjek : TextView
+
+    companion object{
+        const val EXTRA_CARS = "extra_cars"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pindah_objek)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        tvpenerimaobjek=findViewById(R.id.tv_penerima_objek)
+
+        val cars: Cars = intent.getParcelableExtra<Cars>(EXTRA_CARS) as Cars
+        val text = "merk: ${cars.merk.toString()} \nTahun: ${cars.tahun.toString()}\nPlat: ${cars.plat.toString()}"
+        tvpenerimaobjek.text = text
+    }
+}
